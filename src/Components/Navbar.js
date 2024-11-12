4607541012fa2e53d3342a629513bab2623993e3
Biprodeep12 <bipbose123@gmail.com>
2024-11-13 03:13:07 +0530

    up10

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
@@ -64,6 +64,27 @@ export default function Navbar() {
       window.location.href('#skill');
     }
   };
+
+  const images = ['/align-justify-svgrepo-com.svg', '/cross.svg'];
+
+  const [pod, dop] = useState(images[0]);
+  const kal = document.getElementById('kal');
+  const dw = document.getElementById('dwom');
+
+  const toggleStyle = () => {
+    if (pod === images[0]) {
+      kal.style.width = '50px';
+      kal.style.height = '50px';
+      dop(images[1]);
+      dw.style.visibility = 'visible';
+    } else {
+      kal.style.width = '70px';
+      kal.style.height = '70px';
+      dop(images[0]);
+      dw.style.visibility = 'hidden';
+    }
+  };
+
   return (
     <>
       <div>
@@ -87,6 +108,26 @@ export default function Navbar() {
             <Link to='/Proj' className='proj' id='ani'>
               Projects
               <div id='under'></div>
+            </Link>
+          </div>
+          <div className='dropd' onClick={toggleStyle}>
+            <img src={pod} id='kal' alt='icon' />
+          </div>
+          <div className='dwom' id='dwom'>
+            <Link to='/' id='tion1'>
+              Home
+            </Link>
+            <div onClick={Ab} id='tion1'>
+              About
+            </div>
+            <div onClick={sk} id='tion1'>
+              Skills
+            </div>
+            <Link to='/Proj' id='tion1'>
+              Projects
+            </Link>
+            <Link to='/Cont' id='tion1'>
+              Contacts
             </Link>
           </div>
         </nav>
